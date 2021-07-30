f4e5ab971fb78727462a7cd2b9294f75eb8ceb53
Toan Le <thanhtoanhcm1602@gmai.com>
2021-07-30 09:59:26 +0700

    implement learning fabricjs

---
+++ index.js
@@ -0,0 +1,12 @@
+var canvas = new fabric.Canvas('canvas', {
+  width: 500,
+  height: 500,
+  backgroundColor: "red"
+});
+
+canvas.renderAll();
+
+// fabric.Image.fromURL('https://www.google.com/url?sa=i&url=https%3A%2F%2F1hot.vn%2Fdanh-sach-10-dia-diem-co-phong-canh-dep-nhat-the-gioi.html&psig=AOvVaw23rveMAKN6GtcHMCarL2lo&ust=1627696241819000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCICg5JTXifICFQAAAAAdAAAAABAD1', (img) => {
+//   canvas.setBackgroundImage = img
+//   canvas.renderAll()
+// });
