98b7ac068bc9af7319cac8cac808599a1b7c8a31
Toan Le <thanhtoanhcm1602@gmai.com>
2021-07-30 13:28:57 +0700

    - implement function initCanvas.

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-var canvas = new fabric.Canvas("canvas", {
-  width: 500,
-  height: 500,
-  // backgroundColor: "red"
-});
+// var canvas = new fabric.Canvas("canvas", {
+//   width: 500,
+//   height: 500,
+//   // backgroundColor: "red"
+// });
 
-canvas.renderAll();
+// canvas.renderAll();
 
-fabric.Image.fromURL("https://picsum.photos/id/237/500/500", function (img) {
+// fabric.Image.fromURL("https://picsum.photos/id/237/500/500", function (img) {
+//     img.set({
+//         width : canvas.width,
+//         height : canvas.height
+//     });
+//   canvas.setBackgroundImage(img);
+//   canvas.renderAll();
+//   //   canvas.add(img).renderAll().setActiveObject(img);
+// });
+// ---------------
+var initCanvas = (id) => {
+  return new fabric.Canvas(id, {
+    width: 500,
+    height: 500,
+  });
+};
+
+const setBackground = (url, canvas) => {
+  fabric.Image.fromURL(url, function (img) {
     img.set({
-        width : canvas.width,
-        height : canvas.height
+      width: canvas.width,
+      height: canvas.height,
     });
-  canvas.setBackgroundImage(img);
-  canvas.renderAll();
-  //   canvas.add(img).renderAll().setActiveObject(img);
-});
+    canvas.setBackgroundImage(img);
+    canvas.renderAll();
+  });
+};
+
+var canvas = initCanvas("canvas");
+
+setBackground("https://picsum.photos/id/237/500/500", canvas);
